749879fa828872da6a1e5a212799c243c92e4fbf
MariaFariss <maria.fariss.etu@univ-lemans.fr>
2023-03-13 00:02:30 +0100

    commit

--- Assets/Scripts/GhoulScript.cs
+++ Assets/Scripts/GhoulScript.cs
@@ -44,3 +44,40 @@ public class GhoulScript : MonoBehaviour
         }
     }
 }
+
+
+//using UnityEngine;
+
+//public class GhoulScript : MonoBehaviour
+//{
+//    private Animation animations;
+
+//    void Start()
+//    {
+//        animations = gameObject.GetComponent<Animation>();
+//    }
+
+//    void Update()
+//    {
+//        // Vérification si l'utilisateur touche l'écran
+//        if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began)
+//        {
+//            Ray ray = Camera.main.ScreenPointToRay(Input.GetTouch(0).position); // Rayon depuis la caméra vers le point de touché
+//            RaycastHit hit; // Informations sur l'objet touché
+
+//            // Détection de l'objet touché
+//            if (Physics.Raycast(ray, out hit))
+//            {
+//                if (hit.transform.gameObject == gameObject)
+//                {
+//                    animations.Play("Death");
+//                }
+//            }
+//        }
+
+//        if (animations.isPlaying && animations["Death"].time >= animations["Death"].length)
+//        {
+//            animations.Stop();
+//        }
+//    }
+//}
